e5831d9b388478f3e0c6ab3de5f9e7d2f51d3e30
taoufikcherroud@gmail.com <taoufikcherroud@gmail.com>
2022-11-19 23:11:16 +0100

    Query class

---
+++ neo4j-parsing/src/main/java/nodes/Query.java
@@ -0,0 +1,10 @@
+package nodes;
+
+public class Query {
+   public String query;
+    public Query(String query) {
+        this.query = query;
+    }
+
+    //TODO PUT INSERT INTO NEO4JDATABASE METHOD IN QUERY CLASS
+}
