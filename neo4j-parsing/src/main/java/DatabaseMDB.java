1fb2053b0ddd58ea747944f4762d56f49f867212
@Flynn <flynn.filip.steppe@vub.be>
2022-11-24 17:23:40 +0100

    Added code for loading data  from a CSV file

--- neo4j-parsing/src/main/java/DatabaseMDB.java
+++ neo4j-parsing/src/main/java/DatabaseMDB.java
@@ -3,101 +3,96 @@ import java.io.*;
 
 public class DatabaseMDB {
 
-    // JDBC driver name and database URL
-
-    static final String JDBC_DRIVER = "org.mariadb.jdbc.Driver";
-    static final String DB_URL = "jdbc:mariadb://192.168.100.174/db";
-
-    static String csvFile = "journal.csv";
-    int batchSize = 20;
-
-    //  Database credentials
     static final String USER = "peanut222link";
     static final String PASS = "password";
+    static final String CSV_FILE = "journal.csv";
 
-    public static void readFromCSV() throws FileNotFoundException {
+    int batchSize = 20;
+
+    public static void createTables(Connection connection) {
+        Statement statement = null;
+        try {
+            System.out.println("Creating table in given database...");
+            statement = connection.createStatement();
+
+            // PUT ALL THE DIFFERENT TABLES HERE
+
+            String journalTable = "CREATE TABLE JOURNAL "
+                    + "(article_key VARCHAR(255) not NULL,"
+                    + "title VARCHAR(255));";
+
+            statement.executeUpdate(journalTable);
+            System.out.println("Created tables in given database...");
+        } catch (SQLException e) {
+            //Handle errors for JDBC
+            e.printStackTrace();
+        }
+    }
+
+    public static void readFromCSV(Connection connection) {
 
         try {
-            String sql = "INSERT INTO review (course_name, student_name, timestamp, rating, comment) VALUES (?, ?, ?, ?, ?)";
+            connection.setAutoCommit(false);
+            String sql = "INSERT INTO JOURNAL (article_key, title) VALUES (?, ?)";
+            PreparedStatement statement = connection.prepareStatement(sql);
 
-            BufferedReader lineReader = new BufferedReader(new FileReader(csvFile));
-            String lineText = null;
+            BufferedReader lineReader = new BufferedReader(new FileReader(CSV_FILE));
+            int batchSize = 20;
+            String lineText;
 
-            int count = 0;
+            int counter = 0;
 
             lineReader.readLine(); // skip header line
 
             while ((lineText = lineReader.readLine()) != null) {
                 String[] data = lineText.split(",");
+
                 String title = data[0];
                 String article_key = data[1];
-                System.out.println(title + " " + article_key);
+
+                statement.setString(1, article_key);
+                statement.setString(2, title);
+
+                statement.addBatch();
+                counter += 1;
+
+                if (counter % batchSize == 0) {
+                    statement.executeBatch();
+                }
             }
-
             lineReader.close();
-
-        } catch (IOException e) {
+            statement.executeBatch();
+            connection.commit();
+        } catch (IOException | SQLException e) {
             throw new RuntimeException(e);
         }
-
     }
 
     // https://stackoverflow.com/questions/37909487/how-can-i-connect-to-mariadb-using-java
 
     public static void main() throws FileNotFoundException {
         Connection connection = null;
-        Statement statement = null;
-
-        readFromCSV();
-
-        /* try {
-            //STEP 3: Open a connection
+        try {
             System.out.println("Connecting to a selected database...");
-            connection = DriverManager.getConnection(
-                    "jdbc:mariadb://localhost:3306/databaseDIM", USER, PASS);
+            connection = DriverManager.getConnection("jdbc:mariadb://localhost:3306/databaseDIM", USER, PASS);
             System.out.println("Connected database successfully...");
 
-            //STEP 4: Execute a query
-            System.out.println("Creating table in given database...");
-            statement = connection.createStatement();
+            createTables(connection);
+            readFromCSV(connection);
 
-            String sql = "CREATE TABLE JOURNAL "
-                    + "(article_key VARCHAR(255) not NULL,"
-                    + "title VARCHAR(255),"
-                    + "PRIMARY KEY (article_key));";
-
-            statement.executeUpdate(sql);
-            System.out.println("Created table in given database...");
-        } catch (SQLException se) {
+        } catch (SQLException e) {
             //Handle errors for JDBC
-            se.printStackTrace();
-        } catch (Exception e) {
-            //Handle errors for Class.forName
             e.printStackTrace();
         } finally {
-            //finally block used to close resources
-            try {
-                if (statement != null) {
-                    connection.close();
-                }
-            } catch (SQLException se) {
-            }
             try {
                 if (connection != null) {
                     connection.close();
                 }
-            } catch (SQLException se) {
-                se.printStackTrace();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
             }
         }
-        System.out.println("Goodbye!"); */
-
-
+        System.out.println("Goodbye!");
     }
-
-    // Procedure for creating the tables
-
-
-    // Procedure for reading the csv files into the right tables
 
 }
