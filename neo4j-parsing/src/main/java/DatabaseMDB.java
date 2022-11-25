7045c855d67726df5a4d511d6725bef9c4854d1c
zolar <ounnoughi.fzohra@gmail.com>
2022-11-25 10:14:11 +0100

    created sql tables for mariadDB.

--- neo4j-parsing/src/main/java/DatabaseMDB.java
+++ neo4j-parsing/src/main/java/DatabaseMDB.java
@@ -5,37 +5,82 @@ public class DatabaseMDB {
     // JDBC driver name and database URL
 
     static final String JDBC_DRIVER = "org.mariadb.jdbc.Driver";
-    static final String DB_URL = "jdbc:mariadb://192.168.100.174/db";
+    static final String DB_URL = "jdbc:mariadb://localhost:3306/dblp";
 
     //  Database credentials
-    static final String USER = "peanut222link";
-    static final String PASS = "password";
+    static final String USER = "root";
+    static final String PASS = "0000";
 
     // https://stackoverflow.com/questions/37909487/how-can-i-connect-to-mariadb-using-java
 
-    public void main() {
+    public void createTables () {
         Connection connection = null;
         Statement statement = null;
         try {
             //STEP 3: Open a connection
             System.out.println("Connecting to a selected database...");
             connection = DriverManager.getConnection(
-                    "jdbc:mariadb://localhost:3306/databaseDIM", USER, PASS);
+                    DB_URL, USER, PASS);
             System.out.println("Connected database successfully...");
 
             //STEP 4: Execute a query
             System.out.println("Creating table in given database...");
             statement = connection.createStatement();
 
-            String sql = "CREATE TABLE REGISTRATION "
-                    + "(id INTEGER not NULL, "
-                    + " first VARCHAR(255), "
-                    + " last VARCHAR(255), "
-                    + " age INTEGER, "
-                    + " PRIMARY KEY ( id ))";
+            String sql_article = "CREATE TABLE article "
+                    + "(article_key VARCHAR(255) not NULL, "
+                    + " authors VARCHAR(255), "
+                    + " title VARCHAR(255), "
+                    + " journal VARCHAR(255), "
+                    + " year VARCHAR(255), "
+                    + " pages VARCHAR(255), "
+                    + " volume VARCHAR(255), "
+                    + " number VARCHAR(255)) ";
 
-            statement.executeUpdate(sql);
-            System.out.println("Created table in given database...");
+            String sql_proceeding = "CREATE TABLE proceedings "
+                    + "(proceeding_key VARCHAR(255) not NULL, "
+                    + " editor VARCHAR(255), "
+                    + " title VARCHAR(255), "
+                    + " booktitle VARCHAR(255), "
+                    + " year VARCHAR(255), "
+                    + " volume VARCHAR(255))";
+
+            String sql_inproceeding = "CREATE TABLE inproceedings "
+                    + "(inproceeding_key VARCHAR(255) not NULL, "
+                    + " title VARCHAR(255), "
+                    + " booktitle VARCHAR(255), "
+                    + " year VARCHAR(255), "
+                    + " pages VARCHAR(255)) ";
+
+            String sql_author = "CREATE TABLE author "
+                    + "(article_key VARCHAR(255) not NULL, "
+                    + " inproceeding_key VARCHAR(255) not NULL,"
+                    + " name VARCHAR(255) )";
+
+            String sql_editor = "CREATE TABLE editor "
+                    + "(proceeding_key VARCHAR(255) not NULL,"
+                    + " name VARCHAR(255) )";
+
+            String sql_journal = "CREATE TABLE journal "
+                    + "(article_key VARCHAR(255) not NULL, "
+                    + " title VARCHAR(255))";
+
+            String sql_conferences = "CREATE TABLE conferences "
+                    + "(proceeding_key VARCHAR(255) not NULL, "
+                    + " title VARCHAR(255))";
+
+
+
+
+            statement.executeUpdate(sql_article);
+            statement.executeUpdate(sql_proceeding);
+            statement.executeUpdate(sql_inproceeding);
+            statement.executeUpdate(sql_author);
+            statement.executeUpdate(sql_editor);
+            statement.executeUpdate(sql_journal);
+            statement.executeUpdate(sql_conferences);
+
+            System.out.println("Created all table in given database successfully!");
         } catch (SQLException se) {
             //Handle errors for JDBC
             se.printStackTrace();
@@ -58,6 +103,6 @@ public class DatabaseMDB {
                 se.printStackTrace();
             }
         }
-        System.out.println("Goodbye!");
+        System.out.println("All done!");
     }
 }
