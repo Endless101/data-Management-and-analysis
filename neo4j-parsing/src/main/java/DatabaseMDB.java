6e215cb39b7e7b6b461f29e175bc5478bb8ca22e
@Flynn <flynn.filip.steppe@vub.be>
2022-12-05 20:14:31 +0100

    Added the code for loading all the data into Mariadb

--- neo4j-parsing/src/main/java/DatabaseMDB.java
+++ neo4j-parsing/src/main/java/DatabaseMDB.java
@@ -5,9 +5,6 @@ public class DatabaseMDB {
 
     static final String USER = "peanut222link";
     static final String PASS = "password";
-    static final String CSV_FILE = "journal.csv";
-
-    int batchSize = 20;
 
     public static void createTables(Connection connection) {
         Statement statement = null;
@@ -15,13 +12,50 @@ public class DatabaseMDB {
             System.out.println("Creating table in given database...");
             statement = connection.createStatement();
 
-            // PUT ALL THE DIFFERENT TABLES HERE
+            String sql_article = "CREATE TABLE articles "
+                    + "(article_key VARCHAR(255) not NULL, "
+                    + " authors VARCHAR(255), "
+                    + " title VARCHAR(255), "
+                    + " journal VARCHAR(255), "
+                    + " year VARCHAR(255), "
+                    + " pages VARCHAR(255), "
+                    + " volume VARCHAR(2040), "
+                    + " number VARCHAR(255)) ";
+            String sql_proceeding = "CREATE TABLE proceedings "
+                    + "(proceeding_key VARCHAR(255) not NULL, "
+                    + " editor VARCHAR(255), "
+                    + " title VARCHAR(255), "
+                    + " booktitle VARCHAR(255), "
+                    + " year VARCHAR(255), "
+                    + " volume VARCHAR(2040))";
+            String sql_inproceeding = "CREATE TABLE inproceedings "
+                    + "(inproceeding_key VARCHAR(255) not NULL, "
+                    + " title VARCHAR(255), "
+                    + " booktitle VARCHAR(255), "
+                    + " year VARCHAR(255), "
+                    + " pages VARCHAR(255)) ";
+            String sql_author = "CREATE TABLE authors "
+                    + "(article_key VARCHAR(255) not NULL, "
+                    + " inproceeding_key VARCHAR(255) not NULL,"
+                    + " name VARCHAR(255) )";
+            String sql_editor = "CREATE TABLE editors "
+                    + "(proceeding_key VARCHAR(255) not NULL,"
+                    + " name VARCHAR(255) )";
+            String sql_journal = "CREATE TABLE journals "
+                    + "(article_key VARCHAR(255) not NULL, "
+                    + " title VARCHAR(255))";
+            String sql_conference = "CREATE TABLE conferences "
+                    + "(proceeding_key VARCHAR(255) not NULL, "
+                    + " title VARCHAR(255))";
 
-            String journalTable = "CREATE TABLE JOURNAL "
-                    + "(article_key VARCHAR(255) not NULL,"
-                    + "title VARCHAR(255));";
+            statement.executeUpdate(sql_article);
+            statement.executeUpdate(sql_proceeding);
+            statement.executeUpdate(sql_inproceeding);
+            statement.executeUpdate(sql_author);
+            statement.executeUpdate(sql_editor);
+            statement.executeUpdate(sql_journal);
+            statement.executeUpdate(sql_conference);
 
-            statement.executeUpdate(journalTable);
             System.out.println("Created tables in given database...");
         } catch (SQLException e) {
             //Handle errors for JDBC
@@ -29,33 +63,23 @@ public class DatabaseMDB {
         }
     }
 
-    public static void readFromCSV(Connection connection) {
-
+    public static void readFromCSV(Connection connection, String csv,  String sql, int amount) {
         try {
             connection.setAutoCommit(false);
-            String sql = "INSERT INTO JOURNAL (article_key, title) VALUES (?, ?)";
             PreparedStatement statement = connection.prepareStatement(sql);
-
-            BufferedReader lineReader = new BufferedReader(new FileReader(CSV_FILE));
+            BufferedReader lineReader = new BufferedReader(new FileReader(csv));
             int batchSize = 20;
             String lineText;
-
             int counter = 0;
-
             lineReader.readLine(); // skip header line
 
             while ((lineText = lineReader.readLine()) != null) {
                 String[] data = lineText.split(",");
-
-                String title = data[0];
-                String article_key = data[1];
-
-                statement.setString(1, article_key);
-                statement.setString(2, title);
-
+                for( int i = 1; i <= amount; i++) {
+                    statement.setString(i, data[i-1]);
+                }
                 statement.addBatch();
                 counter += 1;
-
                 if (counter % batchSize == 0) {
                     statement.executeBatch();
                 }
@@ -68,6 +92,46 @@ public class DatabaseMDB {
         }
     }
 
+    public static void loadArticle(Connection connection) {
+        readFromCSV(connection, "article.csv","INSERT INTO articles (article_key, authors, title, journal, year, pages, volume, number ) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", 8);
+    }
+    public static void loadProceeding(Connection connection) {
+        readFromCSV(connection, "proceedings.csv","INSERT INTO proceedings (proceeding_key, editor, title, booktitle, year, volume) VALUES (?, ?, ?, ?, ?, ?)", 6);
+    }
+    public static void loadInproceeding(Connection connection) {
+        readFromCSV(connection, "inproceeding.csv","INSERT INTO inproceedings (inproceeding_key, title, booktitle, year, pages) VALUES (?, ?, ?, ?, ?)", 5);
+    }
+    public static void loadAuthor(Connection connection) {
+        readFromCSV(connection, "author.csv","INSERT INTO authors (article_key, inproceeding_key, name) VALUES (?, ?, ?)", 3);
+    }
+    public static void loadEditor(Connection connection) {
+        readFromCSV(connection, "editor.csv", "INSERT INTO editors (proceeding_key, name) VALUES (?, ?)", 2);
+    }
+    public static void loadJournal(Connection connection) {
+        readFromCSV(connection, "journal.csv", "INSERT INTO journals (article_key, title) VALUES (?, ?)", 2);
+    }
+    public static void loadConference(Connection connection) {
+        readFromCSV(connection, "conference.csv","INSERT INTO conferences (proceeding_key, title) VALUES (?, ?)", 2);
+    }
+
+    public static void loadData(Connection connection) {
+        System.out.println("Started loading Data");
+        loadArticle(connection);
+        System.out.println("Loaded articles");
+        loadProceeding(connection);
+        System.out.println("Loaded proceedings");
+        loadInproceeding(connection);
+        System.out.println("Loaded inproceedings");
+        loadAuthor(connection);
+        System.out.println("Loaded authors");
+        loadEditor(connection);
+        System.out.println("Loaded editors");
+        loadJournal(connection);
+        System.out.println("Loaded journals");
+        loadConference(connection);
+        System.out.println("Loaded conferences");
+    }
+
     // https://stackoverflow.com/questions/37909487/how-can-i-connect-to-mariadb-using-java
 
     public static void main() throws FileNotFoundException {
@@ -76,10 +140,8 @@ public class DatabaseMDB {
             System.out.println("Connecting to a selected database...");
             connection = DriverManager.getConnection("jdbc:mariadb://localhost:3306/databaseDIM", USER, PASS);
             System.out.println("Connected database successfully...");
-
             createTables(connection);
-            readFromCSV(connection);
-
+            loadData(connection);
         } catch (SQLException e) {
             //Handle errors for JDBC
             e.printStackTrace();
@@ -94,5 +156,4 @@ public class DatabaseMDB {
         }
         System.out.println("Goodbye!");
     }
-
 }
